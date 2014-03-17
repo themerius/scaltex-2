b9e1457683d83e1e53964f1b4b75660f1fcc65a8
Sven Hodapp <sven.hodapp@gmail.com>
2014-03-17 08:56:18 +0100

    Now using jade templates

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+var socket;
+
+if (!window.WebSocket) {
+  window.WebSocket = window.MozWebSocket;
+}
+
+if (window.WebSocket) {
+  socket = new WebSocket("ws://localhost:8000/echo");
+  socket.onmessage = function(event) {
+    var json = JSON.parse(event.data);
+    if (json.from == 2)
+      document.getElementById("inputbx").value = json.content;  // here should the content with variables...
+    document.getElementById("entity"+json.from).innerHTML = event.data;
+  };
+  socket.onopen = function(event) { 
+    document.getElementById("response").innerHTML = "Opened"; 
+  };
+  socket.onclose = function(event) { 
+    document.getElementById("response").innerHTML = "Closed"; 
+  };
+} else {
+  alert("Your browser does not support Web Socket.");
+}
+
+var updateEntity = function (form) {
+  socket.send(form.inputbox.value)
+}
