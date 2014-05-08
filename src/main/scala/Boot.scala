932848b0802e0c86a5e9f39efab6804db6cd5b77
Sven Hodapp <sven.hodapp@gmail.com>
2014-05-08 13:41:18 +0200

    Now runs with and without couchdb

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -40,65 +40,77 @@ object Boot {
     Factory.system = Config.actorSystem
     Factory.updater = Config.actorSystem.actorOf(Props[UpdaterActor], "updater")
 
-    val url = "http://127.0.0.1:5984/test_document/_all_docs"
-    val jsonAllDocs = dijon.parse(HTTP.get(url).getTextBody)  // load the testset from db
-    val actorAndNext = ListBuffer[Tuple2[ActorRef, String]]()
-    for (row <- jsonAllDocs.rows.toSeq) {
-      val id = row.id.as[String].get
-      val jsonDoc = dijon.parse(HTTP.get("http://127.0.0.1:5984/test_document/" + id).getTextBody)
-      val nextId = jsonDoc.next.as[String].get
-      val content = jsonDoc.content.as[String].get
-      val cls = jsonDoc.classDef.as[String].get
-      val actor = Factory.makeWithIdAndNextRef[EntityActor](id, content)
-      actor ! Msg.ClassDef(cls)
-      actorAndNext += Tuple2(actor, nextId)
-    }
+    def loadFromCouchDB = {
+      val url = "http://127.0.0.1:5984/test_document/_all_docs"
+      val jsonAllDocs = dijon.parse(HTTP.get(url).getTextBody)  // load the testset from db
+      val actorAndNext = ListBuffer[Tuple2[ActorRef, String]]()
+      for (row <- jsonAllDocs.rows.toSeq) {
+        val id = row.id.as[String].get
+        val jsonDoc = dijon.parse(HTTP.get("http://127.0.0.1:5984/test_document/" + id).getTextBody)
+        val nextId = jsonDoc.next.as[String].get
+        val content = jsonDoc.content.as[String].get
+        val cls = jsonDoc.classDef.as[String].get
+        val actor = Factory.makeWithIdAndNextRef[EntityActor](id, content)
+        actor ! Msg.ClassDef(cls)
+        actorAndNext += Tuple2(actor, nextId)
+      }
 
-    import scala.concurrent.duration._
-    import scala.concurrent.{Future, Await}
-    import scala.language.postfixOps
+      import scala.concurrent.duration._
+      import scala.concurrent.{Future, Await}
+      import scala.language.postfixOps
 
-    for (an <- actorAndNext) {
-      val actor = an._1
-      val next = an._2
-      if (next != "") {
-        // create an ActorSelection based on the path
-        val sel = Factory.system.actorSelection(s"/user/$next");
-        // check if a single actor exists at the path
-        val fut: Future[ActorRef] = sel.resolveOne(100 millis);
-        val ref = Await.result(fut, 100 millis);
+      for (an <- actorAndNext) {
+        val actor = an._1
+        val next = an._2
+        if (next != "") {
+          // create an ActorSelection based on the path
+          val sel = Factory.system.actorSelection(s"/user/$next");
+          // check if a single actor exists at the path
+          val fut: Future[ActorRef] = sel.resolveOne(100 millis);
+          val ref = Await.result(fut, 100 millis);
 
-        actor ! Msg.Next(ref)
+          actor ! Msg.Next(ref)
+        }
       }
     }
 
-    // val source = scala.io.Source.fromFile(getClass.getResource("/plot.py").toURI)
-    // val plotpy = source.mkString
-    // source.close()
+    def loadFromCode = {
+      val source = scala.io.Source.fromFile(getClass.getResource("/plot.py").toURI)
+      val plotpy = source.mkString
+      source.close()
 
-    // Factory.makeEntityActor[EntityActor] ! Msg.Content("Introduction")
+      Factory.makeEntityActor[EntityActor] ! Msg.Content("Introduction")
 
-    // val n2 = Factory.makeEntityActor[EntityActor]
-    // n2 ! Msg.Content("The heading is ${entity1.heading}!")
-    // n2 ! Msg.ClassDef("Text")
+      val n2 = Factory.makeEntityActor[EntityActor]
+      n2 ! Msg.Content("The heading is ${entity1.heading}!")
+      n2 ! Msg.ClassDef("Text")
 
-    // Factory.makeEntityActor[EntityActor] ! Msg.Content("Experiment")
+      Factory.makeEntityActor[EntityActor] ! Msg.Content("Experiment")
 
-    // val n4 = Factory.makeEntityActor[EntityActor]
-    // n4 ! Msg.Content(
-    //   """url = "http://upload.wikimedia.org/wikipedia/commons/a/a1/""" +
-    //   """Koffein_-_Caffeine.svg", desc = "Strukturformel von Koffein." """)
-    // n4 ! Msg.ClassDef("Figure")
+      val n4 = Factory.makeEntityActor[EntityActor]
+      n4 ! Msg.Content(
+        """url = "http://upload.wikimedia.org/wikipedia/commons/a/a1/""" +
+        """Koffein_-_Caffeine.svg", desc = "Strukturformel von Koffein." """)
+      n4 ! Msg.ClassDef("Figure")
 
-    // val n5 = Factory.makeEntityActor[EntityActor]
-    // n5 ! Msg.Content("Example Python Code")
-    // n5 ! Msg.ClassDef("SubSection")
+      val n5 = Factory.makeEntityActor[EntityActor]
+      n5 ! Msg.Content("Example Python Code")
+      n5 ! Msg.ClassDef("SubSection")
 
-    // val n6 = Factory.makeEntityActor[EntityActor]
-    // n6 ! Msg.Content(plotpy)
-    // n6 ! Msg.ClassDef("Text")
+      val n6 = Factory.makeEntityActor[EntityActor]
+      n6 ! Msg.Content(plotpy)
+      n6 ! Msg.ClassDef("Text")
 
-    // Factory.makeEntityActor[EntityActor] ! Msg.Content("Summary")
+      Factory.makeEntityActor[EntityActor] ! Msg.Content("Summary")
+    }
+
+    try {
+      loadFromCouchDB
+    } catch {
+      case e: java.net.ConnectException =>
+        println("CouchDB not available.")
+        loadFromCode
+    }
   }
 
   def main(args: Array[String]) {
@@ -172,15 +184,19 @@ class EchoWebSocketActor extends WebSocketAction {
   }
 
   def persistEntityStateIntoCouchDB(json: String) = future {
-    val jsonObj = dijon.parse(json)
-    val url = "http://127.0.0.1:5984/test_document/entity" + jsonObj.from.as[Double].get.toInt
-    val inDb = HTTP.get(url)
-    if (inDb.getStatus == 200) {
-      val jsonInDb = dijon.parse(inDb.getTextBody)
-      val rev = jsonInDb._rev
-      jsonObj._rev = rev
+    try {
+      val jsonObj = dijon.parse(json)
+      val url = "http://127.0.0.1:5984/test_document/entity" + jsonObj.from.as[Double].get.toInt
+      val inDb = HTTP.get(url)
+      if (inDb.getStatus == 200) {
+        val jsonInDb = dijon.parse(inDb.getTextBody)
+        val rev = jsonInDb._rev
+        jsonObj._rev = rev
+      }
+      var put = HTTP.put(url, jsonObj.toString.getBytes, "text/json").getStatus
+    } catch {
+      case e: java.net.ConnectException => println("CouchDB not available.")
     }
-    var put = HTTP.put(url, jsonObj.toString.getBytes, "text/json").getStatus
   }
 
   override def postStop() {
