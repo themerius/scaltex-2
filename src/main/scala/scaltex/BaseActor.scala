a608d6e78c4996a23a973d39f52727e7945a64cb
Sven Hodapp <sven.hodapp@gmail.com>
2014-05-14 13:48:50 +0200

    Refactored BaseActor

--- src/main/scala/scaltex/BaseActor.scala
+++ src/main/scala/scaltex/BaseActor.scala
@@ -3,11 +3,12 @@ package scaltex
 import akka.actor.Actor
 import akka.actor.ActorRef
 import akka.actor.ActorSelection
+
 import com.github.pathikrit.dijon.`{}`
 
 import Messages._
 
-abstract class BaseActor(updater: ActorRef) extends Actor {
+abstract class BaseActor(updater: ActorRef) extends Actor with DiscoverReferences {
 
   val availableDocElems: Map[String, DocumentElement]
 
@@ -21,42 +22,44 @@ abstract class BaseActor(updater: ActorRef) extends Actor {
   this.state.contentEval = ""
 
   def receive = {
-    case Change(to)   => this.state.documentElement = to
-    case Next(id)     => this.state.next = id
-    case Previous(id) => this.state.previous = id
-    case m @ M(to, jsonMsg) =>
-      if (to.contains(assignedDocElem))
-        documentElement._processMsg(jsonMsg, refs)
-      else
-        if (refs.nextExisting) next ! m
-    case State => updater ! CurrentState(currentState.toString)
-    case Update => documentElement._gotUpdate(refs)
-      val triggered = triggerRequestForCodeGen(findAllActorRefs(state.contentSrc.as[String].get))
-      if (!triggered) updater ! CurrentState(currentState.toString)
-    case Content(content) => this.state.contentSrc = content
+    case Change(to) =>
+      `change current doc elem`(to)
+
+    case Next(id) =>
+      `change next ref`(id)
+
+    case Previous(id) =>
+      `change previous ref`(id)
+
+    case m: M =>
+      `let doc elem process the msg`(m)
+
+    case Update =>
+      `let doc elem update, trigger code gen, send curr state`
+
+    case Content(content) =>
+      `change content src`(content)
+
     case RequestForCodeGen(requester, others) =>
-      requester ! ReplyForCodeGen(genCode, others.size == 0)
-      if (others.size > 0) {
-        val head = context.actorSelection("../" + others.head)
-        head ! RequestForCodeGen(requester, others.tail)
-      }
+      `reply with code, pass request along`(requester, others)
+
     case ReplyForCodeGen(code, replyEnd) =>
-      replyCodeBuffer += code
-      if (replyEnd) triggerInterpreter
-    case ReturnValue(repr) => this.state.contentRepr = repr.toString
-      updater ! CurrentState(currentState.toString)
+      `buffer code then trigger interpretation`(code, replyEnd)
+
+    case ReturnValue(repr) =>
+      `change content repr, send curr state`(repr)
   }
 
+  def id = this.state._id.as[String].get
+  def refs: Refs = new Refs(next, updater, self)
+  def currentState = documentElement.state ++ this.state
   def assignedDocElem = this.state.documentElement.as[String].get
 
-  def id = this.state._id.as[String].get
-
-  def documentElement: DocumentElement = {
+  def documentElement: DocumentElement =
     if (availableDocElems.contains(assignedDocElem))
       availableDocElems(assignedDocElem)
     else
       new EmptyDocumentElement
-  }
 
   def next: ActorSelection = {
     val next = this.state.next.as[String].get
@@ -68,54 +71,52 @@ abstract class BaseActor(updater: ActorRef) extends Actor {
     context.actorSelection(if (prev == "") "" else "../" + prev)
   }
 
-  def refs: Refs = new Refs(next, updater, self)
-
-  def currentState = documentElement.state ++ this.state
-
-  def findAllActorRefs(str: String) = {
-    val allIds = "id_[\\$a-zA-Z0-9]*_id".r.findAllIn(str)
-    val withCuttedIds = allIds.map(x => x.slice(3, x.size - 3))
-    withCuttedIds.toList
+  private def `let doc elem process the msg`(m: M): Unit = {
+    if (m.to.contains(assignedDocElem))
+      documentElement._processMsg(m.jsonMsg, refs)
+    else if (refs.nextExisting) next ! m
   }
 
-  def triggerRequestForCodeGen(refs: List[String]): Boolean = {
-    if (refs.size > 0) {
-      val firstActorRef = context.actorSelection("../" + refs.head)
-      val codeGenRequest = RequestForCodeGen(self, refs.tail)
-      firstActorRef ! codeGenRequest
-      true
-    } else {
-      this.state.contentRepr = this.state.contentSrc
-      false
+  private def `let doc elem update, trigger code gen, send curr state`: Unit = {
+    documentElement._gotUpdate(refs)
+    val contentSrc = state.contentSrc.as[String].get
+    val allRefs = findAllActorRefs(in = contentSrc)
+    val triggered = triggerRequestForCodeGen(allRefs)
+    if (!triggered) updater ! CurrentState(currentState.toString)
+  }
+
+  private def `reply with code, pass request along`(requester: ActorRef, others: List[String]): Unit = {
+    requester ! ReplyForCodeGen(genCode, others.size == 0)
+    if (others.size > 0) {
+      val head = context.actorSelection("../" + others.head)
+      head ! RequestForCodeGen(requester, others.tail)
     }
   }
 
-  def genCode = {
-    val actorRefName = "id_" + this.id + "_id"
-    val code = s"""
-      | val $actorRefName = new ${documentElement.getClass.getName}
-      | $actorRefName.state = json\"\"\" ${documentElement.state} \"\"\"
-    """.stripMargin
-    code
+  private def `buffer code then trigger interpretation`(code: String, replyEnd: Boolean): Unit = {
+    replyCodeBuffer += code
+    if (replyEnd) triggerInterpreter
   }
 
-  val replyCodeBuffer = scala.collection.mutable.Buffer[String]()
+  private def `change content repr, send curr state`(repr: Any): Unit = {
+    this.state.contentRepr = repr.toString
+    updater ! CurrentState(currentState.toString)
+  }
 
-  def triggerInterpreter = {
-    val references = replyCodeBuffer.toSet.mkString("\n")
-    replyCodeBuffer.clear
+  private def `change content src`(content: String): Unit = {
+    this.state.contentSrc = content
+  }
 
-    // Note: this.state.contentSrc delivers already quotes -> "..."
-    val content = "s\"\"" + this.state.contentSrc + "\"\""
-    val completeCode = s"""
-      | import com.github.pathikrit.dijon.JsonStringContext
-      | ${references}
-      | val contentRepr = ${content}
-      | contentRepr
-    """.stripMargin
+  private def `change previous ref`(id: String): Unit = {
+    this.state.previous = id
+  }
 
-    val interpreterActor = context.actorSelection("../interpreter")
-    interpreterActor ! Interpret(completeCode, "contentRepr")
+  private def `change next ref`(id: String): Unit = {
+    this.state.next = id
+  }
+
+  private def `change current doc elem`(to: String): Unit = {
+    this.state.documentElement = to
   }
 
 }
---
+++ src/main/scala/scaltex/DiscoverReferences.scala
@@ -0,0 +1,53 @@
+package scaltex
+import Messages._
+import akka.actor.ActorSelection.toScala
+
+trait DiscoverReferences {
+  this: BaseActor =>
+
+  def findAllActorRefs(in: String) = {
+    val allIds = "id_[\\$a-zA-Z0-9]*_id".r.findAllIn(in)
+    val withCuttedIds = allIds.map(x => x.slice(3, x.size - 3))
+    withCuttedIds.toList
+  }
+
+  def triggerRequestForCodeGen(refs: List[String]): Boolean = {
+    if (refs.size > 0) {
+      val firstActorRef = context.actorSelection("../" + refs.head)
+      val codeGenRequest = RequestForCodeGen(self, refs.tail)
+      firstActorRef ! codeGenRequest
+      true
+    } else {
+      this.state.contentRepr = this.state.contentSrc
+      false
+    }
+  }
+
+  def genCode = {
+    val actorRefName = "id_" + this.id + "_id"
+    val code = s"""
+      | val $actorRefName = new ${documentElement.getClass.getName}
+      | $actorRefName.state = json\"\"\" ${documentElement.state} \"\"\"
+    """.stripMargin
+    code
+  }
+
+  val replyCodeBuffer = scala.collection.mutable.Buffer[String]()
+
+  def triggerInterpreter = {
+    val references = replyCodeBuffer.toSet.mkString("\n")
+    replyCodeBuffer.clear
+
+    // Note: this.state.contentSrc delivers already quotes -> "..."
+    val content = "s\"\"" + this.state.contentSrc + "\"\""
+    val completeCode = s"""
+      | import com.github.pathikrit.dijon.JsonStringContext
+      | ${references}
+      | val contentRepr = ${content}
+      | contentRepr
+    """.stripMargin
+
+    val interpreterActor = context.actorSelection("../interpreter")
+    interpreterActor ! Interpret(completeCode, "contentRepr")
+  }
+}
